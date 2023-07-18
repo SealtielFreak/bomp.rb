2733bf864798517ca66d1b72f85ac53b55d0ceb5
SealtielFreak <Die.Seal_Valderrama@outlook.com>
2023-07-18 02:33:06 -0600

    Minor changes.

--- sample/hello_world.rb
+++ sample/hello_world.rb
@@ -6,10 +6,18 @@ include Bomp
 
 world = World.new 640, 480
 
-world.add(Rect[0, 0, 10, 10])
-world.add(Rect[2, 3, 10, 10])
-world.add(Rect[300, 300, 10, 10])
+world.insert(Rect[0, 0, 10, 10])
+world.insert(Rect[2, 3, 10, 10])
+world.insert(Rect[300, 300, 10, 10])
 
-world.sort.each do |i|
-	puts i.to_s
+puts 'Current position: ' + world[0].position.to_s
+cols = world.move(0, Vector2[3, -1]) do |item, other|
+	puts 'Other collision is ' + other.to_s
+	:push
+end
+
+puts 'Current position: ' + world[0].position.to_s
+
+cols.each do |col|
+	puts 'Collision: ' + col.to_s
 end
