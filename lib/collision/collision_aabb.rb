b2eb6481996bf106a2b206436842f03f9026dda0
SealtielFreak <Die.Seal_Valderrama@live.com>
2024-03-24 22:10:26 -0600

    Refactoring modules of collisions

--- lib/collision/collision_aabb.rb
+++ lib/collision/collision_aabb.rb
@@ -2,20 +2,42 @@
 
 module Bomp
   module CollisionAABB
+    # Check if it was aligned horizontally.
+    # @param [Bomp::Rect] a
+    # @param [Bomp::Rect] b
+    # @return [TrueClass, FalseClass]
     def self.was_horizontal_aligned?(a, b)
       a.left < b.right && a.right > b.left
     end
 
+    # Check if it was aligned vertically.
+    # @param [Bomp::Rect] a
+    # @param [Bomp::Rect] b
+    # @return [TrueClass, FalseClass]
     def self.was_vertical_aligned?(a, b)
       a.top < b.bottom && a.bottom > b.top
     end
 
+    # Check if overlaps
+    # @param [Bomp::Rect] a
+    # @param [Bomp::Rect] b
+    # @return [TrueClass, FalseClass]
     def self.is_overlaps?(a, b)
       was_vertical_aligned?(a, b) && was_horizontal_aligned?(a, b)
     end
 
-    def self.bounce(item, other, goal) end
+    # Rebound reaction
+    # @param [Bomp::Rect] a
+    # @param [Bomp::Rect] b
+    # @return [Bomp::Rect]
+    def self.bounce(item, other, goal)
+      item
+    end
 
+    # Rebound reaction
+    # @param [Bomp::Rect] a
+    # @param [Bomp::Rect] b
+    # @return [Bomp::Rect]
     def self.cross(item, other, goal)
       x, y = goal.to_a
 
@@ -32,8 +54,18 @@ module Bomp
       item
     end
 
-    def self.touch(item, other, goal) end
+    # Touch reaction
+    # @param [Bomp::Rect] a
+    # @param [Bomp::Rect] b
+    # @return [Bomp::Rect]
+    def self.touch(item, other, goal)
+      item
+    end
 
+    # Slide reaction
+    # @param [Bomp::Rect] a
+    # @param [Bomp::Rect] b
+    # @return [Bomp::Rect]
     def self.slide(item, other, goal)
       x, y = goal.to_a
 
@@ -58,6 +90,10 @@ module Bomp
       item
     end
 
+    # Push reaction
+    # @param [Bomp::Rect] a
+    # @param [Bomp::Rect] b
+    # @return [Bomp::Rect]
     def self.push(item, other, goal)
       x, y = goal.to_a
 
