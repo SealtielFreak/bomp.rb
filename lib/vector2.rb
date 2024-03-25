de5704ec96ecb1be36edba2c475ca0bfcd836efb
SealtielFreak <Die.Seal_Valderrama@live.com>
2024-03-24 22:26:04 -0600

    Written documentation of Rect and Vector2

--- lib/vector2.rb
+++ lib/vector2.rb
@@ -4,31 +4,52 @@ module Bomp
   class Vector2
     attr_accessor :x, :y
 
+    # Initialize vector 2d
+    # @param [Integer] x
+    # @param [Integer] y
     def initialize(x, y)
       @x = x
       @y = y
     end
 
+    # Plus operator
+    # @param [Array, Bomp::Vector2] other
+    # @return [Bomp::Vector2]
     def +(other)
       Vector2[@x + other[0], @y + other[1]]
     end
 
+    # Minus operator
+    # @param [Array, Bomp::Vector2] other
+    # @return [Bomp::Vector2]
     def -(other)
       Vector2[@x - other[0], @y - other[1]]
     end
 
+    # Multiply Operator
+    # @param [Integer] scalar
+    # @return [Bomp::Vector2]
     def *(scalar)
       Vector2[@x * scalar, @y * scalar]
     end
 
+    # Division Operator
+    # @param [Integer] scalar
+    # @return [Bomp::Vector2]
     def /(scalar)
       Vector2[@x / scalar, @y / scalar]
     end
 
+    # Equals Operator
+    # @param [Array, Bomp::Vector2] other
+    # @return [TrueClass, FalseClass]
     def ==(other)
       @x == other[0] && @y == other[1]
     end
 
+    # Get (x, y) of vector 2d
+    # @param [Integer] index
+    # @return [Integer, NilClass]
     def [](index)
       case index
       when 0 then @x
@@ -37,6 +58,10 @@ module Bomp
       end
     end
 
+    # Set (x, y) of vector 2d
+    # @param [Integer] index
+    # @param [Integer] value
+    # @return [Integer, NilClass]
     def []=(index, value)
       case index
       when 0 then @x = value
@@ -44,18 +69,28 @@ module Bomp
       end
     end
 
+    # Cast to string
+    # @return [String]
     def to_s
       self.to_a.to_s
     end
 
+    # Cast to array
+    # @return [[Integer, Integer]]
     def to_a
       [@x, @y]
     end
 
+    # Get auto sum of vector
+    # @return [Integer]
     def sum
       @x + @y
     end
 
+    # Create vector 2d like array
+    # @param [Integer] x
+    # @param [Integer] y
+    # @return [Bomp::Vector2]
     def self.[](x, y)
       Vector2.new(x, y)
     end
